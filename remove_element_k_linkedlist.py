d13f493c98a214ecb7ae0a4ba346afaac5b1914a
Abobakr Hussein Osman <abobakrhussein.sh@gmail.com>
2020-07-20 17:04:11 +0200

    Day 20

---
+++ remove_element_k_linkedlist.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head == None :
+            return head
+        prev , current = None , head
+        while current != None:
+            if current.val == val:
+                if prev == None:
+                    head = current.next
+                else:
+                    prev.next = current.next
+            else:
+                prev = current
+            current = current.next
+        return head
